e24e989e0357422f503c0cf67a3ff25e761dc11f
kpaek-ias <kpaek@integralads.com@integralads.com>
2024-09-05 13:12:20 -1000

    mass sprite cleanup

--- sprite.js
+++ sprite.js
@@ -7,7 +7,7 @@ if (!fs.existsSync('out/sprite')) {
   });
 }
 
-// slice('Screenshot_20240904-104508.png');
+// slice('common.png');
 
 const files = fs.readdirSync('in/sprite');
 files.forEach(async fileName => {
@@ -20,23 +20,28 @@ console.log('done');
 async function slice(fileName) {
   console.log(fileName);
   await sharp(`in/sprite/${fileName}`)
-    .extract({ left: 1102, top: 416, width: 208, height: 265 })
-    .toFormat('webp')
-    .toFile(`out/sprite/${fileName.replace('png', 'webp')}`)
-    // .toBuffer({ resolveWithObject: true })
-    // .then(async ({ data, info }) => {
-    //   const { width, height, channels } = info;
-    //   const newData = convertToRawData(data, width, height);
-    //   eraseBackground2(newData, width, height);
-    //   const rebuiltData = convertToBuffer(newData)
-    //   await sharp(rebuiltData, { raw: { width, height, channels } })
-    //     .resize(264, 264, {
-    //       fit: 'contain',
-    //       background: 'transparent'
-    //     })
-    //     .toFormat('webp')
-    //     .toFile(`out/sprite/${fileName}.webp`)
-    // })
+    // .extract({ left: 1935, top: 311, width: 210, height: 210 })
+    .ensureAlpha()
+    // .resize(70, 70, {
+    .resize(804, 360, {
+      fit: 'contain',
+      background: 'transparent',
+      kernel: 'nearest',
+    })
+    // .toFile(`out/sprite/${fileName}`)
+    .raw()
+    .toBuffer({ resolveWithObject: true })
+    .then(async ({ data, info }) => {
+      const { width, height, channels } = info;
+      const newData = convertToRawData(data, width, height);
+      eraseBackground2(newData, width, height);
+      // colorBlackAndWhite(newData);
+      const rebuiltData = convertToBuffer(newData)
+      await sharp(rebuiltData, { raw: { width, height, channels } })
+        // .toFormat('webp')
+        // .toFile(`out/sprite/${fileName.replace('png', 'webp')}`)
+        .toFile(`out/sprite/${fileName}`)
+    })
 }
 
 function convertToRawData(rawData) {
@@ -52,6 +57,19 @@ function convertToRawData(rawData) {
   return newData
 }
 
+function convertToBuffer(data) {
+  const buffer = [];
+  for (let i = 0; i < data.length; i++) {
+    buffer.push(data[i].r);
+    buffer.push(data[i].g);
+    buffer.push(data[i].b);
+    buffer.push(data[i].a);
+  }
+  return Buffer.from(buffer);
+}
+
+// function resizeImage
+
 function eraseBackground2(data, width, height) {
   eraseNonBlackRegion(data, width, height);
   eraseSmallBlackRegions(data, width, height);
@@ -65,13 +83,16 @@ function eraseNonBlackRegion(data, width, height) {
       continue;
     }
     const n = curr.y * width + curr.x;
-    if (pixelIsBlack(data[n])) {
+    if (isBlackOrWhite(data[n])) {
       continue;
     }
     if (data[n].v1) {
       continue;
     }
     data[n].v1 = true;
+    data[n].r = 0;
+    data[n].g = 255;
+    data[n].b = 0;
     data[n].a = 0;
     candidates.push({ x: curr.x + 1, y: curr.y });
     candidates.push({ x: curr.x - 1, y: curr.y });
@@ -85,7 +106,7 @@ function eraseSmallBlackRegions(data, width, height) {
   for (let y = 0; y < height; y++) {
     for (let x = 0; x < width; x++) {
       const n = y * width + x;
-      if (pixelIsBlack(data[n])) {
+      if (isBlackOrWhite(data[n])) {
         blackPixels.push({ x, y, n });
       }
     }
@@ -93,7 +114,7 @@ function eraseSmallBlackRegions(data, width, height) {
 
   for (let i = 0; i < blackPixels.length; i++) {
     const region = expandRegion(blackPixels[i].x, blackPixels[i].y, data, width, height);
-    if (region.length !== 0 && region.length < 100) {
+    if (region.length !== 0 && region.length < 10) {
       for (let j = 0; j < region.length; j++) {
         const n = region[j].y * width + region[j].x;
         data[n].a = 0;
@@ -127,24 +148,39 @@ function expandRegion(x, y, data, width, height) {
   return region;
 }
 
-function pixelIsBlack(pixel) {
-  if (pixel.r > 30 || pixel.g > 30 || pixel.b > 30) {
-    return false;
+function colorBlackAndWhite(data) {
+  for (let i = 0; i < data.length; i++) {
+    if (isBlackOrWhite(data[i])) {
+      data[i].r = 0;
+      data[i].g = 255;
+      data[i].b = 0;
+    }
   }
-  return (pixel.r + pixel.g + pixel.b) / 3 < 26;
+}
+
+function isBlackOrWhite(pixel) {
+  // black
+  if (pixel.r <= 30 || pixel.g <= 30 || pixel.b <= 30) {
+    return true;
+  }
+  if ((pixel.r + pixel.g + pixel.b) / 3 < 26) {
+    return true;
+  }
+
+  // white
+  if (pixel.r > 240 && pixel.g > 240 && pixel.b > 240) {
+    return true
+  }
+  if ((pixel.r + pixel.g + pixel.b) / 3 > 245) {
+    return true;
+  }
+
+  return false;
 }
 
 function isTransparent(pixel) {
-  return pixel.a == 0;
-}
-
-function convertToBuffer(data) {
-  const buffer = [];
-  for (let i = 0; i < data.length; i++) {
-    buffer.push(data[i].r);
-    buffer.push(data[i].g);
-    buffer.push(data[i].b);
-    buffer.push(data[i].a);
+  if (pixel.r === 0 && pixel.g === 255 && pixel.b === 0) {
+    return true;
   }
-  return Buffer.from(buffer);
+  return pixel.a === 0;
 }
